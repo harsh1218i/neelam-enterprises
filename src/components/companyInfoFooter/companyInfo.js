877701635efaec9a04f9fd9d104b9df23a972e8e
Harshvardhan Singh Chauhan <chauhanharshvardhansingh4@gmail.com>
2024-11-03 11:13:09 +0530

    changes

--- src/components/companyInfoFooter/companyInfo.js
+++ src/components/companyInfoFooter/companyInfo.js
@@ -1,5 +1,5 @@
 import SocialMediaLinks from "../socialMediaLinks/socialMediaLinks";
-import options from "../utilities/contact-us";
+import options from "../../utilities/options";
 // import style from "./CompanyInfo.module.scss"
 
 export default function CompanyInfo() {
--- src/components/contactUsForm/contactUsBanner.js
+++ src/components/contactUsForm/contactUsBanner.js
@@ -1,6 +1,6 @@
 "use client"
 import { usePathname } from "next/navigation";
-import options from "../utilities/contact-us";
+import options from "../../utilities/contact-us";
 
 export default function ContactUsBanner() {
     const pathName = usePathname();

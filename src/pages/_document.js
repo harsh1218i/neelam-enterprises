b224f22e569facb6a064094f8774235ec771b86b
Harshvardhan Singh Chauhan <chauhanharshvardhansingh4@gmail.com>
2025-04-25 15:15:00 +0530

    google analytics code added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,14 +7,20 @@ class MyDocument extends Document {
       <Html lang="en"> {/* Add the lang attribute here */}
         <Head>
           {/* Additional <head> tags can be added here */}
+          {/* Google Analytics */}
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-GQ9PN4RXQG"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-
-            gtag('config', 'G-GQ9PN4RXQG');
-          </script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-GQ9PN4RXQG', {
+                page_path: window.location.pathname,
+              });
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />

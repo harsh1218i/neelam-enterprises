8e095410ad9d474acf8eb735ababae58b1f5f899
Harshvardhan Singh Chauhan <chauhanharshvardhansingh4@gmail.com>
2025-05-02 00:39:28 +0530

    Fix: Prevent localhost data in Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,24 +3,29 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
+    const isProduction = process.env.NODE_ENV === 'production';
+
     return (
-      <Html lang="en"> {/* Add the lang attribute here */}
+      <Html lang="en">
         <Head>
-          {/* Additional <head> tags can be added here */}
-          {/* Google Analytics */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-GQ9PN4RXQG"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-GQ9PN4RXQG', {
-                page_path: window.location.pathname,
-              });
-            `,
-            }}
-          />
+          {/* Google Analytics - Only in production */}
+          {isProduction && (
+            <>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-GQ9PN4RXQG"></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-GQ9PN4RXQG', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
